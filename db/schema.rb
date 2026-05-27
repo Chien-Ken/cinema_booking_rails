c06c90cfd7155c8782f628e22ef78f5d65ba96bd
Chien-Ken <chiencntthanu@gmail.com>
2026-05-27 10:35:01 +0700

    update environment variables

--- db/schema.rb
+++ db/schema.rb
@@ -138,6 +138,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_20_075100) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
